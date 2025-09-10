d255fd68a3c8568108451205c5f2bb178c6df76d
Shubham Daule <shubhamdaule@gmail.com>
2025-09-10 02:52:28 +0000

    lets make these expand buttons invisible, user should only see this opti

--- src/components/dashboard/charts/chart-card-header.tsx
+++ src/components/dashboard/charts/chart-card-header.tsx
@@ -29,7 +29,7 @@ export const ChartCardHeader = ({ title, description, Icon, onExpand, isExpandab
                 {isExpandable && onExpand && (
                     <Tooltip>
                         <TooltipTrigger asChild>
-                            <Button variant="ghost" size="icon" className="h-8 w-8 -mt-1 -mr-2" onClick={onExpand}>
+                            <Button variant="ghost" size="icon" className="h-8 w-8 -mt-1 -mr-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300" onClick={onExpand}>
                                 <Expand className="h-4 w-4" />
                             </Button>
                         </TooltipTrigger>
